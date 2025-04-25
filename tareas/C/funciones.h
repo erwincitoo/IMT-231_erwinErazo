5368fd65de91b22a2f7655bbbb20d2207144de61
Erwincitoo <erwinerazo17@gmail.com>
2025-04-24 22:09:11 -0400

    funciones.h file.

---
+++ tareas/C/funciones.h
@@ -0,0 +1,13 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+int parImpar(int num);
+int digitos(int num);
+int suma(int a, int b);
+int resta(int a, int b);
+int multiplicacion(int a, int b);
+int comparacion(int a, int b);
+int factorial(int num);
+int invertir(int num);
+
+#endif
